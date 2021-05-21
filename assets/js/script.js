2473790c142fc3aa1342fe77261b20f82a5d18f6
habibHadjar <lhp6.hhadjar@gmail.com>
2021-05-21 16:55:38 +0200

    panier fini + json full id

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ fetch(FileJson)
 .then(function(item){
     let col1 = document.createElement('div')
     col1.classList.add('col-4')
-    col1.innerHTML = 
-        `
+    col1.insertAdjacentHTML('afterbegin',
+    `
         <div id="${item.vinyl[0].id}" data-name="${item.vinyl[0].vinyl_name}" data-price="${item.vinyl[0].price}" data-img="${item.vinyl[0].album_cover}" class="card h-100">
             <img src="${item.vinyl[0].vinyl_cover}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -19,12 +19,13 @@ fetch(FileJson)
         </div>
         </div>
         `
+        )
         myrow1.append(col1)
 
     let col2 = document.createElement('div')
     col2.classList.add('col-4')
-    col2.innerHTML = 
-        `
+    col2.insertAdjacentHTML('afterbegin',
+    `
         <div id="${item.equip[0].id}" data-name="${item.equip[0].product}" data-price="${item.equip[0].price}" data-img="${item.equip[0].front}" class="card h-100">
             <img src="${item.equip[0].angle}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -37,12 +38,13 @@ fetch(FileJson)
         </div>
         </div>
         `
+        )
         myrow1.append(col2)
 
     let col3 = document.createElement('div')
     col3.classList.add('col-4')
-    col3.innerHTML = 
-        `
+    col3.insertAdjacentHTML('afterbegin',
+    `
         <div id="${item.merch[0].id}" data-name="${item.merch[0].product}" data-price="${item.merch[0].price}" data-img="${item.merch[0].shirt}" class="card h-100">
             <img src="${item.merch[0].shirt}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -55,10 +57,14 @@ fetch(FileJson)
         </div>
         </div>
         `
+        )
         myrow1.append(col3)
 
 
+
         var Cart = []
+        var total
+
 
     var allAddButtons = document.querySelectorAll('[data-add]')
     allAddButtons.forEach(element => {
@@ -83,6 +89,7 @@ fetch(FileJson)
                         console.table(Cart)
                         element[3]++
                         console.table(Cart)
+                        total = Math.round( (total + parseFloat(element[2])) * 100) / 100
                     }
 
                 })
@@ -97,7 +104,8 @@ fetch(FileJson)
                 
                         Cart.push(Product)
                 
-                        console.table(Cart )
+                        console.table(Cart)
+                        total = Math.round((total + parseFloat(idParentOfBtn.dataset.price)) * 100) / 100
                     }
                 
             } else { // Première fois que l'on arrive dans le panier (vide)
@@ -112,6 +120,8 @@ fetch(FileJson)
         
                 console.table(Cart )
 
+                total = parseFloat(idParentOfBtn.dataset.price)
+
             }
 
 
@@ -122,25 +132,79 @@ fetch(FileJson)
                 myrow2.removeChild(myrow2.firstChild)
             }            
 
-            Cart.forEach(element => {
+            Cart.forEach((element,i) => {
+                console.log(total)
 
                 let row = document.createElement('div')
                 row.classList.add('row')
                 myrow2.append(row)
     
-                row.innerHTML = 
-                `
+                row.insertAdjacentHTML('afterbegin',
+                    `
                 <div data-cart-item="${element[0]}" data class="row">
                 <div class="col d-flex justify-content-around">
                     <div><img src="${element[4]}"></div>
                     <div data-cart-name="${element[1]}">${element[1]}</div>
                     <div data-cart-price="${element[2]}">${element[2]}</div>
-                    <div data-cart-qt="${element[3]}">${element[3]}</div>
+                    <div data-cart-qt-id="${element[0]}" data-cart-qt="${element[3]}">${element[3]}</div>
                     <div><button data-plus="${element[0]}">+</button><button data-minus="${element[0]}">-</button></div>
                     <div><button data-trash="${element[0]}">TRASH</button></div>
+
                 </div>
                 </div>
                 `
+                )
+                
+                let btnPlus = document.querySelector(`[data-plus="${element[0]}"`)
+                let btnMinus = document.querySelector(`[data-minus="${element[0]}"`)
+                let divQt = document.querySelector(`[data-cart-qt-id="${element[0]}"`)
+
+                // console.log(btnPlus.dataset.plus)
+                // console.log(divQt.dataset.cartQt)
+
+                btnPlus.addEventListener('click', function(){
+                    if(element[3]++ < 10){
+                        // console.log(element[3])
+                        element[3]
+                        total = Math.round( (total + parseFloat(element[2])) * 100) / 100
+                        // console.log(element[3])
+                        divQt.dataset.cartQt = element[3]
+                        divQt.innerHTML = element[3]
+                        // console.log(divQt.dataset.cartQt)
+                        // console.table(element)
+                        console.table(Cart)
+                        console.log(total)
+
+                    } else {
+                        alert('Vous ne pouvez achetez plus de 10 fois le même article')
+                    }
+                })
+
+                btnMinus.addEventListener('click', function(){
+                    if(element[3]-- > 1){
+                        // console.log(element[3])
+                        element[3]
+                        // console.log(element[3])
+                        divQt.dataset.cartQt = element[3]
+                        divQt.innerHTML = element[3]
+                        // console.log(divQt.dataset.cartQt)
+                        // console.table(element)
+                        console.table(Cart)
+                        total = Math.round( (total - parseFloat(element[2])) * 100) / 100
+                        console.log(total)
+                        
+                    } else {
+                        let minusBtn = document.querySelector(`[data-minus=${CSS.escape(element[0])}]`)
+                        let idParentOfMinus = document.querySelector(`[data-cart-item=${CSS.escape(minusBtn.dataset.minus)}]`)
+                        // console.log(idParentOfMinus)
+                        idParentOfMinus.remove()
+                        // console.table(Cart)
+                        Cart.splice(i,1)
+                        total = Math.round( (total - parseFloat(element[2])) * 100) / 100
+                        console.table(Cart)
+                        console.log(total)
+                    }
+                })
             })
 
             var TrashButtons = document.querySelectorAll('[data-trash]')
@@ -152,66 +216,24 @@ fetch(FileJson)
 
                 Cart.forEach((element,i)=>{
                     if(element[0] == idParentOfTrash.dataset.cartItem){
+                        total = Math.round((total - parseFloat(element[3]) * parseFloat(element[2])) * 100) / 100
                         Cart.splice(i,1)
+                        
                     }
                 })
                 console.log(Cart)
+                console.log(total)
+
 
               })  
             })
 
-            var PlusButtons = document.querySelectorAll('[data-plus]')
-            var MinusButtons = document.querySelectorAll('[data-minus]')
-
-            console.log(PlusButtons)
-            console.log(MinusButtons)
-
-
-            PlusButtons.forEach(element=>{
-                let valueBtnPlus = element.dataset.plus
-                console.log(valueBtnPlus)
-
-                element.addEventListener('click', function(){
-                    Cart.forEach(item =>{
-                        if(item[0] == valueBtnPlus){
-                            item[3]++
-
-                            while(myrow2.hasChildNodes()){
-                                myrow2.removeChild(myrow2.firstChild)
-                            }
-                            Cart.forEach(element => {
-
-                                let row = document.createElement('div')
-                                row.classList.add('row')
-                                myrow2.append(row)
-                    
-                                row.innerHTML = 
-                                `
-                                <div data-cart-item="${element[0]}" data class="row">
-                                <div class="col d-flex justify-content-around">
-                                    <div><img src="${element[4]}"></div>
-                                    <div data-cart-name="${element[1]}">${element[1]}</div>
-                                    <div data-cart-price="${element[2]}">${element[2]}</div>
-                                    <div data-cart-qt="${element[3]}">${element[3]}</div>
-                                    <div><button data-plus="${element[0]}">+</button><button data-minus="${element[0]}">-</button></div>
-                                    <div><button data-trash="${element[0]}">TRASH</button></div>
-                                </div>
-                                </div>
-                                `
-                            })
-                            console.table(Cart)
-                        }
-                    })
-                })
-            })
-
-
-
-
-
         })
+
     })
+
 })
+
 .catch(function(){
     console.log('error')
 })
